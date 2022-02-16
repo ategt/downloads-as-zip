3a189ddabdc6ce7d3d7e603ba4809de4b396811d
Adam Tegtmeier <ategtmei@gmail.com>
2022-02-16 00:26:53 -0500

    Got a test that seems to be working, not sure what it proves, thought.

--- source/index.test.js
+++ source/index.test.js
@@ -1,28 +1,31 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { saveUrls, saveArchive } from './index';
+import FileSaver from 'file-saver';
+// import * as main from './index';
 
+// const saveUrls = main.saveUrls; 
 mocha.setup('bdd');
-let sandbox;
 
 describe('Index', () => {
-  beforeEach(() => {
-    sandbox = sinon.createSandbox();
-  });
-
-  afterEach(() => {
-  	sandbox.restore();
-  });
-
   describe("Save URL's (saveUrls)", () => {
-    it('list of known urls', () => {
-    	const mockSaveArchive = sandbox.stub(saveArchive);
-    	saveUrls(['http://127.0.0.1:5000/gasket-1.webp', 
+    it('list of known urls', (done) => {
+    	//const mockSaveArchive = sinon.replace(FileSaver, "saveAs", sinon.fake());
+      //saveArchive = sinon.fake();
+      const mockSaveAs = sinon.stub(FileSaver, "saveAs");
+
+    	mockSaveAs.callsFake((content, fileName) => {
+        expect(mockSaveAs.called).to.be.true;
+        done();
+      });
+
+      saveUrls(['http://127.0.0.1:5000/gasket-1.webp', 
     			  'http://127.0.0.1:5000/gasket-2.webp',
     			  'http://127.0.0.1:5000/gasket-3.webp',
     			  'http://127.0.0.1:5000/gasket-4.webp']);
 
-    	console.log(mockSaveArchive);
+    	console.log(mockSaveAs);
+      //const x = expect(mockSaveArchive.calledOnce).true();
     });
   });
 });
