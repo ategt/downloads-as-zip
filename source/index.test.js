28f28359e043002a6d13eb96e256f8b4224a0dca
Adam Tegtmeier <ategtmei@gmail.com>
2022-02-18 20:39:34 -0500

    Stitched together the so answers that I needed.  Now to taylor them for what I want.

--- source/index.test.js
+++ source/index.test.js
@@ -13,6 +13,10 @@ import gasketPng from '!!url-loader!./test-assets/images/gasket.png';
 import gasketWebp from '!!url-loader!./test-assets/images/gasket.webp';
 import duskJpg from '!!url-loader!./test-assets/images/dusk-sm.jpg';
 
+/**
+*  Based on the choosen answer at
+*  stackoverflow.com/questions/57929718/loading-binary-file-with-webpack-and-converting-to-blob  
+*/
 const convertToBlob = (rawDataString) => {
   const [match, contentType, base64] = rawDataString.match(/^data:(.+);base64,(.*)$/);
 
@@ -26,6 +30,32 @@ const convertToBlob = (rawDataString) => {
   // Fire the event
   $("#file-chooser").trigger(event);
 };
+
+/**
+*  Taken from
+*  https://stackoverflow.com/a/20151856
+*/
+function base64toBlob(base64Data, contentType) {
+    contentType = contentType || '';
+    var sliceSize = 1024;
+    var byteCharacters = atob(base64Data);
+    var bytesLength = byteCharacters.length;
+    var slicesCount = Math.ceil(bytesLength / sliceSize);
+    var byteArrays = new Array(slicesCount);
+
+    for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+        var begin = sliceIndex * sliceSize;
+        var end = Math.min(begin + sliceSize, bytesLength);
+
+        var bytes = new Array(end - begin);
+        for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
+            bytes[i] = byteCharacters[offset].charCodeAt(0);
+        }
+        byteArrays[sliceIndex] = new Uint8Array(bytes);
+    }
+    return new Blob(byteArrays, { type: contentType });
+}
+
 
 mocha.setup('bdd');
 
