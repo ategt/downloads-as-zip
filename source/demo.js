ecaeeab5fc1ba25eb0545fe1cdac5d141a7d9f93
Adam Tegtmeier <ategtmei@gmail.com>
2022-02-23 17:11:05 -0500

    More code cleanup. Testsstill passing.

--- source/demo.js
+++
@@ -1,5 +0,0 @@
-import { saveUrls } from './index';
-
-const gallery = document.getElementById("gallery-2");
-const imgs = gallery.getElementsByTagName("img");
-saveUrls(imgs.map(img => img.src.replace("-200x250","")));
